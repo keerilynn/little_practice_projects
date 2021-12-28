0abd3589eaecbe5328cc7d084db78c09765b28aa
Keeri Tramm <35671162+keerilynn@users.noreply.github.com>
2021-12-28 11:35:58 -0600

    .startswith
    A quick .startswith exercise

---
+++ startswith.py
@@ -0,0 +1,13 @@
+continents = [
+    'Asia',
+    'South America',
+    'North America',
+    'Africa',
+    'Europe',
+    'Antarctica',
+    'Australia',
+]
+
+for continent in continents:
+    if continent.startswith("A"):
+        print(continent)
