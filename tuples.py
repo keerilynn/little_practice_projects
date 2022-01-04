1b843a6416c9f24fcfb63eba7f43327c4d226a36
Keeri Tramm <35671162+keerilynn@users.noreply.github.com>
2022-01-04 14:08:57 -0600

    Add files via upload
    refresh on python data types/structures. Study guide in code.

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#practicing tuples
+
+tuple_1 = 1, 2, 3, 4, 5, 6, 7
+
+tuple_2 = ('cat', 'dog', 'fish', 'horse')
+
+print(tuple_1)
+print(tuple_2)
+
+# study guide created here: https://kinopio.club/python-data-structures-La53-n6-4nRXfKxDm7lzh
